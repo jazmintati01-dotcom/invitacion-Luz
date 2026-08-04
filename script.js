52b08b1dcc17dc14d03d7392a29206791e30121d
jazmintati01-dotcom <jazmintati01@gmail.com>
2026-08-04 18:41:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirInvitacion(){
+
+    document.querySelector(".portada")
+    .classList.add("oculta");
+
+}
+
+
+
+function abrirAlbum(){
+
+    document.getElementById("album")
+    .style.display="block";
+
+}
